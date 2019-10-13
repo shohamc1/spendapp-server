de9f037546e55e3ef0ebe693f3b321f82d09408a
Shoham <shhmchk@gmail.com>
2019-10-13 23:24:40 +0800

    storage is now permanent

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import json
-import sqlite3
 
-conn = sqlite3.connect ('database.db')
-c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS data (id int, name text)')
 
-with open('C:\\react\\PythonBackend\\data.json') as f:
+with open('.\data.json') as f:
     data = json.load(f)
 
+#DELETE
+users = data['data']
+newusers = []
+for testid in range (0, len(users)):
+    if users[testid]['uid'] != 3:
+        newusers.append(users[testid])
+
+users = newusers
+print (newusers)
+
+
+#POST and PUT
+#input_id = input()
+#input_name = input()
+
+#a_dict = {"id": input_id, "name": input_name}
+#data['data'].append(a_dict)
+
+#with open('C:\\react\\PythonBackend\\data.json', 'w') as f:
+#    json.dump(data, f)
+
+#testing stuff
 #print (data['data'][1]['id'])
-for person in data['data']:
-    person_name = person['name']
-    person_id = person['id']
+#for person in data['data']:
+#    person_name = person['name']
+#    person_id = person['id']
 
-    c.execute('INSERT INTO data VALUES (' + person_id + ',"' + person_name + '")')
-    conn.commit()
+#for GET
+#num = (len(data['data']))
 
-conn.close()
+#for id in range (0, num):
+#    if (data['data'][id]["uid"] == 3):
+#        print (data['data'][id]['name'])
--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import os
+import json
 #from run import app
 
 app = Flask(__name__)
 api = Api(app)
 
-users = []
+with open('.\data.json') as f:
+    data = json.load(f)
+    users = data['data']
+#users = []
+#print (users[1])
 
 class User (Resource):
     def get (self, uid):
-        for user in users:
-            if (uid == user["uid"]):
-                return user, 200
+        for testid in range(0, len(users)):
+            if (uid == users[testid]["uid"]):
+                return users[testid], 200
         return "User not found", 404
     
     def post (self, uid):
@@ -20,41 +25,51 @@ class User (Resource):
         parser.add_argument("name")
         args = parser.parse_args()
 
-        for user in users:
-            if (uid == user["uid"]):
+        for testid in range(0, len(users)):
+            if (uid == users[testid]["uid"]):
                 return "User with ID {} already exists".format(uid), 400
         
-        user = {
-            "uid": uid,
-            "name": args["name"]
-        }
-        users.append(user), 201
+        user = {"uid": uid, "name": args["name"]}
+        users.append(user)
+        with open('.\data.json', 'w') as f:
+            json.dump(data, f), 201
 
     def put (self, uid):
         parser = reqparse.RequestParser()
         parser.add_argument("name")
         args = parser.parse_args()
 
-        for user in users:
-            if (uid == user["uid"]):
-                user["name"] = args["name"]
-                return user, 200
+        for testid in range(0, len(users)):
+            if (uid == users[testid]["uid"]):
+                users[testid]["name"] = args["name"]
+                with open('.\data.json', 'w') as f:
+                    json.dump(data, f)
+                return users[testid], 201
         
-        user = {
-            "uid": uid,
-            "name": args["name"]
-        }
+        user = {"uid": uid, "name": args["name"]}
         users.append(user)
-        return user, 201
+        with open('.\data.json', 'w') as f:
+            json.dump(data, f)
+        return "UID {} has been added".format(uid), 201
     
     def delete (self, uid):
         global users
-        users = [user for user in users if user["uid"] != uid]
+        newusers = []
+        for testid in range(0, len(users)):
+            if users[testid]['uid'] != uid:
+                newusers.append(users[testid])
+        #users = [users[testid] for testid in range(0, len(users)) if users[testid]["uid"] != uid]
+        data['data'] = newusers
+        with open('.\data.json', 'w') as f:
+            json.dump(data, f)
         return "{} has been deleted".format(uid), 200
 
 api.add_resource(User, "/user/<int:uid>")
-#app.run(debug=True)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+#for testing
+app.run(debug=True)
+
+#for deployment
+#if __name__ == '__main__':
+#    port = int(os.environ.get('PORT', 5000))
+#    app.run(host='0.0.0.0', port=port)
