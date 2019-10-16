8a4265354b4f31d414cb9157275f126494990868
Shoham <shhmchk@gmail.com>
2019-10-16 21:17:17 +0800

    change to mongoDB

--- server.py
+++ server.py
@@ -2,66 +2,64 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import os
 import json
+import pymongo
 #from run import app
 
 app = Flask(__name__)
 api = Api(app)
 
-with open('data.json') as f:
-    data = json.load(f)
-    users = data['data']
+client = pymongo.MongoClient("mongodb+srv://shohamc1:shohamc19960@cluster0-xcamt.gcp.mongodb.net/test?retryWrites=true&w=majority")
+mydb = client['appdatabase']
+mycol = mydb['names']
+allentries = mycol.find()
+
 #users = []
 #print (users[1])
 
+
+
 class User (Resource):
     def get (self, uid):
-        for testid in range(0, len(users)):
-            if (uid == users[testid]["uid"]):
-                return users[testid], 200
+        allentries = mycol.find()
+        for testuser in allentries:
+            if (uid == testuser["_id"]):
+                return testuser, 200
+        
         return "User not found", 404
     
     def post (self, uid):
+        allentries = mycol.find()
         parser = reqparse.RequestParser()
         parser.add_argument("name")
         args = parser.parse_args()
 
-        for testid in range(0, len(users)):
-            if (uid == users[testid]["uid"]):
+        for testuser in allentries:
+            if (uid == testuser["_id"]):
                 return "User with ID {} already exists".format(uid), 400
         
-        user = {"uid": uid, "name": args["name"]}
-        users.append(user)
-        with open('data.json', 'w') as f:
-            json.dump(data, f), 201
+        user = {"_id": uid, "name": args["name"]}
+        mycol.insert_one(user)
+        return user, 201
 
     def put (self, uid):
+        allentries = mycol.find()
         parser = reqparse.RequestParser()
         parser.add_argument("name")
         args = parser.parse_args()
 
-        for testid in range(0, len(users)):
-            if (uid == users[testid]["uid"]):
-                users[testid]["name"] = args["name"]
-                with open('data.json', 'w') as f:
-                    json.dump(data, f)
-                return users[testid], 201
+        for testuser in allentries:
+            if (uid == testuser["_id"]):
+                testuser["name"] = args["name"]
+                mycol.update_one({"_id": uid},{"$set": {"name": args["name"]}})
+                return testuser, 201
         
-        user = {"uid": uid, "name": args["name"]}
-        users.append(user)
-        with open('data.json', 'w') as f:
-            json.dump(data, f)
+        user = {"_id": uid, "name": args["name"]}
+        mycol.insert_one(user)
         return "UID {} has been added".format(uid), 201
     
     def delete (self, uid):
-        global users
-        newusers = []
-        for testid in range(0, len(users)):
-            if users[testid]['uid'] != uid:
-                newusers.append(users[testid])
-        #users = [users[testid] for testid in range(0, len(users)) if users[testid]["uid"] != uid]
-        data['data'] = newusers
-        with open('data.json', 'w') as f:
-            json.dump(data, f)
+        allentries = mycol.find()
+        mycol.delete_one({"_id": uid})
         return "{} has been deleted".format(uid), 200
 
 api.add_resource(User, "/user/<int:uid>")
